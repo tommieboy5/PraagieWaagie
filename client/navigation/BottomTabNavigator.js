5b26176acb6c0655e3ce0b92ac6cb796b556038e
Giovanni <531406@student.glu.nl>
2020-02-29 14:51:33 +0100

    removed text

--- client/navigation/BottomTabNavigator.js
+++ client/navigation/BottomTabNavigator.js
@@ -8,9 +8,6 @@ const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
 
 export default function BottomTabNavigator({ navigation, route }) {
-  // Set the header title on the parent stack navigator depending on the
-  // currently active tab. Learn more in the documentation:
-  // https://reactnavigation.org/docs/en/screen-options-resolution.html
   navigation.setOptions({ headerTitle: getHeaderTitle(route) });
 
   return (
