110c39991bb89c09a5c1dbdeb6622d33d17e7348
Giovanni <531406@student.glu.nl>
2020-03-01 00:25:24 +0100

    added signout button in app.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import firebase from '../firebase'
+import {Button} from 'semantic-ui-react'
 
 function App() {
+  const signOut = () => firebase.auth().signOut()
   return (
     <div className="App">
-        <p>hallo</p>
+        <Button onClick={() => signOut()}>Signout</Button>
     </div>
   );
 }
