7b009e32ed736497ca9bad44c3e3624a496727f6
Giovanni <531406@student.glu.nl>
2020-02-29 21:09:06 +0100

    added login

--- client/App.js
+++ client/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import LoginScreen from './src/screens/LoginScreen'
+import LoadingScreen from './src/screens/LoadingScreen'
+import DashBoardScreen from './src/screens/DashboardScreen'
+import * as firebase from 'firebase'
+import {firebaseConfig} from './config'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+firebase.initializeApp(firebaseConfig)
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppNavigator/>
+    )
+  }
 }
+
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen:LoadingScreen,
+  LoginScreen: LoginScreen, 
+  DashBoardScreen: DashBoardScreen
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator)
 
 const styles = StyleSheet.create({
   container: {
---
+++ client/config.js
@@ -0,0 +1,10 @@
+  // Your web app's Firebase configuration
+export var firebaseConfig = {
+  apiKey: "AIzaSyBkVxJm01n_8rnzL1wjl3cG0ZhAeN0Z1R0",
+  authDomain: "praagiewaagie.firebaseapp.com",
+  databaseURL: "https://praagiewaagie.firebaseio.com",
+  projectId: "praagiewaagie",
+  storageBucket: "praagiewaagie.appspot.com",
+  messagingSenderId: "53110014268",
+  appId: "1:53110014268:web:3cc38c80f3663f614d1e39"
+};
