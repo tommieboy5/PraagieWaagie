ee9e0d80efe91ab1dedd5bd6aff16224c69c4650
Gio <531406@student.glu.nl>
2020-03-05 14:21:34 +0100

    bug fixes

--- src/Components/screens/Account/UserPanel.js
+++ src/Components/screens/Account/UserPanel.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import firebase from '../../../../firebase'
+import firebase from '../../../firebase'
 
 import {Grid, Header, Icon, Image, Button, Input, Message} from 'semantic-ui-react'
 import {Link, useHistory} from 'react-router-dom'
